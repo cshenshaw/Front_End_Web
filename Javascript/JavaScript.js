7bced2e43f830b25d7f79fdf0642b394d2895715
cshenshaw <charles_charlie@hotmail.co.uk>
2017-03-10 09:43:52 +0000

    Final Edition

--- Javascript/JavaScript.js
+++ Javascript/JavaScript.js
@@ -4,20 +4,20 @@ function showImg(n) {
     var i;
     var x =document.getElementsByClassName("Slides");
     
-    if (n > x.length){
-        slideIndex=1
-    };
+    if (n > x.length) {
+        slideIndex = 1;
+    } 
     
-    if (n < 1){
-        slideIndex = x.length
-    };
+    if (n < 1) {
+        slideIndex = x.length;
+    }
     
     for (i = 0; i < x.length; i++){
         x[i].style.display = "none"; 
     }
     
-    x[slideIndex-1].style.display = "block";
-    x[sLideIndex+1].style.display = "block";
+    x[slideIndex - 1].style.display = "block";
+    x[sLideIndex + 1].style.display = "block";
 }
 
 var slideIndex = 1;
@@ -25,12 +25,14 @@ showImg(slideIndex);
 
 //**************Navigation arrows
 function arrows(n) {
+    "use strict";
     showImg(slideIndex += n);
 }
 
 
 //***********Add up cost of comics
 function checkTotal() {
+    
     document.listForm.total.value = '';
     var sum = parseFloat(0);
 		
@@ -42,13 +44,7 @@ function checkTotal() {
 		document.listForm.total.value = parseFloat(sum).toFixed(2);
 }
 
-//**************Googlemap api
-function createMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(37.402, -122.078),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    }
-    var map = new google.maps.Map(document.getElementById("Gmaps"), mapOptions);
-}
+
+
+
 
